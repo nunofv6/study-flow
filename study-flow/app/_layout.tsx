a9d63814432e690a55ad164772f93f7b9bdf62c9
Nuno Vieira <nunoa.fontesvieira@gmail.com>
2025-05-26 15:29:20 +0100

    Study Planner done

--- study-flow/app/_layout.tsx
+++ study-flow/app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Drawer } from "expo-router/drawer";
 
 export default function RootLayout() {
-  return <Drawer />;
+  return (
+    <Drawer>
+      <Drawer.Screen name="Index" options={{ title: 'Home' }} />
+      <Drawer.Screen name="Dashboard" options={{ title: 'Dashboard' }} />
+      <Drawer.Screen name="Schedule" options={{ title: 'Schedule' }} />
+      <Drawer.Screen name="Calendar" options={{ title: 'Calendar' }} />
+      <Drawer.Screen name="StudyPlan" options={{ title: 'Study Plan' }} />
+      <Drawer.Screen name="Profile" options={{ title: 'Profile' }} />
+      <Drawer.Screen name="Productivity" options={{ title: 'Productivity' }} />
+      <Drawer.Screen name="StudyOverview" options={{ title: 'Study Overview' }} />
+      <Drawer.Screen name="About" options={{ title: 'About' }} />
+    </Drawer>
+  )
 }
